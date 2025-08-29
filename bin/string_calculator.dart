8d4cb4ad20cafd291b1211569fee260e519248e4
AmarjitM13 <mallickamarjit@gmail.com>
2025-08-29 14:25:06 +0530

    added usage in main function

--- bin/string_calculator.dart
+++ bin/string_calculator.dart
@@ -1,5 +1,17 @@
 import 'package:string_calculator/string_calculator.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${StringCalculator().add("")}!');
+  final calc = StringCalculator();
+
+  print(calc.add(""));
+  print(calc.add("1"));
+  print(calc.add("1,5"));
+  print(calc.add("1\n2,3"));
+  print(calc.add("//;\n1;2"));
+
+  try {
+    print(calc.add("1,-2,3,-4"));
+  } catch (e) {
+    print(e);
+  }
 }
